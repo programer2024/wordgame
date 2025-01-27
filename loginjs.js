25cb014f788ee63077de454287c6f839a73be60b
Footballtransfer <154350906+programer2024@users.noreply.github.com>
2025-01-27 16:17:14 +0330

    Update loginjs.js

--- loginjs.js
+++ loginjs.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("container");
 
-    // بررسی وجود اطلاعات کاربر در localStorage
     if (localStorage.getItem("username")) {
         container.classList.add("sign-in-mode");
     }
 
-    // سوئیچ بین فرم ورود و ثبت‌نام
     window.toggle = function () {
         container.classList.toggle("sign-in-mode");
     };
 
-    // عملکرد ثبت‌نام
     const signUpButton = document.querySelector(".sign-up button");
     signUpButton.addEventListener("click", function () {
         const username = document.querySelector(".sign-up input[type='text']").value;
         const email = document.querySelector(".sign-up input[type='email']").value;
         const password = document.querySelector(".sign-up input[type='password']").value;
 
-        // بررسی مقادیر ثبت‌نام برای تکراری بودن
         const storedUsername = localStorage.getItem("username");
         const storedEmail = localStorage.getItem("email");
 
@@ -27,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (email === storedEmail) {
             alert("این ایمیل قبلاً ثبت شده است!");
         } else if (username && email && password) {
-            // ذخیره اطلاعات جدید در localStorage
+    
             localStorage.setItem("username", username);
             localStorage.setItem("email", email);
             localStorage.setItem("password", password);
@@ -38,7 +34,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // عملکرد ورود
     const signInButton = document.querySelector(".sign-in button");
     signInButton.addEventListener("click", function () {
         const username = document.querySelector(".sign-in input[type='text']").value;
